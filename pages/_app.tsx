6cad6ac4552e364f68c531f4af77e21dac8a2739
Divya Babu <divya.vontimitta@gmail.com>
2023-10-11 00:41:27 -0400

    theme changes added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,8 @@ import posthog from 'posthog-js'
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
-import 'react-notion-x/src/styles.css'
+// import 'react-notion-x/src/styles.css'
+import 'styles/_styles.css'
 import 'styles/global.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
@@ -18,6 +19,7 @@ import 'styles/global.css'
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
+import 'styles/theme-1.css'
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
