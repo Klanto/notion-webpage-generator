206cd920f31de05b8c0b6f0e0b00239a20ad2767
sahilklanto <77873849+sahilklanto@users.noreply.github.com>
2023-10-14 15:24:58 +0530

    removed notion id hash from urls

--- lib/config.ts
+++ lib/config.ts
@@ -98,10 +98,7 @@ export const isPreviewImageSupportEnabled: boolean = getSiteConfig(
 )
 
 // Optional whether or not to include the Notion ID in page URLs or just use slugs
-export const includeNotionIdInUrls: boolean = getSiteConfig(
-  'includeNotionIdInUrls',
-  !!isDev
-)
+export const includeNotionIdInUrls: boolean = false;
 
 export const navigationStyle: NavigationStyle = getSiteConfig(
   'navigationStyle',
